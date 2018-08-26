78c1147a091086464cfcc5872c01bf3db98e93e4
Esterling Accime <esterling@brainbytescreative.com>
2018-08-25 23:26:12 -0400

    fix: adding extra params

--- handlers/events.js
+++ handlers/events.js
@@ -1,31 +1,34 @@
 const axios = require('axios');
-const _ = require('lodash');
-const { formatEventsBriteData } = require('./format/formatEventBrite');
+const host = 'https://www.eventbriteapi.com/v3';
 
-const retrieveEvents = async (req, res) => {
-    const value = req.params.q;
+const retrieveEvents =  (req, res) => {
+    const resource = '/events/search';
+    const value = req.query.q || 'javascript';
+    
+    let params = `?q=${value}&categories=102`;
 
-    try {
-        const response = await axios({
-            url: `https://www.eventbriteapi.com/v3/events/search?q=${value}`,
-            method: 'GET',
-            headers: {
-                'Authorization': 'Bearer N2A6M2NFJFNS3JNORWIX'
-            }
-        });
-
-        if (_.isEmpty(response.data.events)) {
-            throw new Error('No events found!');
-        }
-
-        const filterEvents = formatEventsBriteData(response.data.events);
-
-        res.json({
-            events: filterEvents
-        });
-    } catch (error) {
-        res.status(404).json(error);
+    if (req.query.latitude) {
+        params += `&location.latitude=${req.query.latitude}`;
     }
+
+    if (req.query.longitude) {
+        params += `&location.longitude=${req.query.latitude}`;
+    }
+
+    axios({
+        url: `${host}${resource}${params}`,
+        method: 'GET',
+        headers: {
+            'Authorization': 'Bearer N2A6M2NFJFNS3JNORWIX'
+        }
+    })
+    .then((response) => {
+        res.json({
+            ...response.data
+        });
+    })
+    .catch((error) => res.status(501).json(error));
+
 };
 
 
