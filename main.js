3759547737fdba678284f42a021b40d343f77d00
Esterling Accime <esterling@brainbytescreative.com>
2018-08-25 23:58:20 -0400

    fix: adding cors

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+
+app.use(cors());
+app.use(bodyParser());
+app.use(bodyParser());
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 const handlebars = require('./handlers');
 
